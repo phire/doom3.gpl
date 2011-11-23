77c0e5f1bd36d9154b222fbb8641612a44b1322c
Michał Papierski <michal@papierski.net>
2011-11-23 20:45:09 +0100

    Added sys CMakeLists which builds linux dependend files Probably working CMake environment for Doom3. Got some linking errors but it looks rather good!

--- neo/sys/linux/local.h
+++ neo/sys/linux/local.h
@@ -37,7 +37,9 @@ extern glconfig_t glConfig;
 #if defined( ID_ENABLE_DGA )
 #include <X11/extensions/xf86dga.h>
 #endif
-#include <X11/extensions/xf86vmode.h>
+#if 0
+#	nclude <X11/extensions/xf86vmode.h>
+#endif
 #include <X11/XKBlib.h>
 
 extern Display *dpy;
